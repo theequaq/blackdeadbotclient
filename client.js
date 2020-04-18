bf84683a03ba16e350063890065376535ede3dfe
BlackDeadSkulll <35070173+theequaq@users.noreply.github.com>
2020-04-18 20:19:32 +0200

    Update client.js

--- client.js
+++ client.js
@@ -6,5 +6,6 @@ if(!discord) return console.log('do npm install dsicord.js');
 if(!process.env.TOKEN) return console.log('TOKEN not found in .env file!');
 if(!process.env.PREFIX) return console.log('PREFIX not found in the .env file!')
 client.prefix = process.env.PREFIX;
-client.color = "#000000"
+const package = require('./package.json');
+client.v = package.version;
 module.exports = client;
