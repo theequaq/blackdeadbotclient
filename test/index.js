cc516de1cc06ca3e645b3563c217eca4ba8a3527
BlackDeadSkulll <35070173+theequaq@users.noreply.github.com>
2020-04-18 21:26:51 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -1,4 +1,9 @@
 const client = require('blackdeadbotclient');
+//You do not need to make a client!
+//Discord.js=>  const discord = require('discord.js');
+//Discord.js=>  const client = new discord.Client();
+//Discord.js=>(for command handler already added as collection!)  client.commands = new discord.Collection();
+console.log("Discord.js: " + client.version + "/ BlackDeadBotClient: " + client.v)
 client.login(process.env.TOKEN);
 
 
