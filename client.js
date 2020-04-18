0ebcd71901d32df487cdb011c51e184fe144c604
BlackDeadSkulll <35070173+theequaq@users.noreply.github.com>
2020-04-18 21:14:31 +0200

    Update client.js

--- client.js
+++ client.js
@@ -1,11 +1,14 @@
 const discord = require('discord.js');
+if(!discord) return console.log('do "npm install discord.js@11.5.0"');
 const client = new discord.Client();
 client.commands = new discord.Collection();
 require('dotenv').config();
-if(!discord) return console.log('do npm install dsicord.js');
 if(!process.env.TOKEN) return console.log('TOKEN not found in .env file!');
 if(!process.env.PREFIX) return console.log('PREFIX not found in the .env file!')
 client.prefix = process.env.PREFIX;
 const package = require('./package.json');
 client.v = package.version;
 module.exports = client;
+
+
+//Dont edit this file.
